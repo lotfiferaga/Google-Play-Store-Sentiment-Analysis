0a7a6d1920b3e245af8149979a7705571070cdd2
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-18 00:56:51 +0100

    Update Google_Play_Store_Sentiment_Analysis.py

--- Google_Play_Store_Sentiment_Analysis.py
+++ Google_Play_Store_Sentiment_Analysis.py
@@ -15,3 +15,12 @@ data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Translat
 data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Translated_Review"]]
 data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["Translated_Review"]]
 print(data.head())
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.figure(figsize=(15, 10))
+sns.scatterplot(data['Sentiment_Polarity'], data['Sentiment_Subjectivity'], 
+                hue = data['Sentiment'], edgecolor='white', palette="twilight_shifted_r")
+plt.title("Google Play Store Reviews Sentiment Analysis", fontsize=20)
+plt.show() 
+
