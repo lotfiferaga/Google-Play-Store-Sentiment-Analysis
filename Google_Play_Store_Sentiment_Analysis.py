84dd89e293bc44addc18c2160a0bc210b0f55cde
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-18 00:55:50 +0100

    Update Google_Play_Store_Sentiment_Analysis.py

--- Google_Play_Store_Sentiment_Analysis.py
+++ Google_Play_Store_Sentiment_Analysis.py
@@ -8,3 +8,10 @@ print(data.isnull().sum())
 
 data = data.dropna()
 print(data.isnull().sum())
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sentiments = SentimentIntensityAnalyzer()
+data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Translated_Review"]]
+data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Translated_Review"]]
+data["Neutral"] = [sentiments.polarity_scores(i)["neu"] for i in data["Translated_Review"]]
+print(data.head())
